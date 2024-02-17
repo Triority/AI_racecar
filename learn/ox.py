53d4402e08c91700019e717cb289ffbda7069120
qwqpap <74455027+qwqpap@users.noreply.github.com>
2024-02-18 00:40:36 +0800

    Initial commit

--- learn/ox.py
+++ learn/ox.py
@@ -38,17 +38,28 @@ class ooxx_machine():
                 self.race[location[0]][location[1]] = 2
             else:
                 raise ValueError("this location has been used")
-
+        return "fin"
 
 class DlModel:
     def __init__(self):
         self.learn_rate = 0.1
         self.rand_poss = 0.05
         self.net_values = []
-    def refresh_net(self,race:list,value:float):
-        hash_value = hash(race)
-        if hash_value in self.net_values:
-            pass
+        self.default_value = 0.8
+        self.need_been_change_value = 0.8
+
+# 我对强化学习的理解还不够透彻
+    def refresh_net(self, race:list, value:float, flag:bool):
+        # 传参： 赛场情况 需要更新的价值（在此赛场情况之前的价值） （本赛场）是否获胜
+        hash_value = hash(str(race))
+        if hash_value not in self.net_values:
+            self.net_values.append([hash_value, self.default_value])
+        else:
+
+
+            now_value = self.net_values[hash_value]
+            now_value = now_value + (now_value - self.last_value)/self.learn_rate
+
 
 
 
